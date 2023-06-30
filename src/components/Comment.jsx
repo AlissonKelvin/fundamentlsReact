fbab8aa7aed1da8a4372be61f7b956cf53427aa8
alissonKelvin <alisson.kelvindecarvalho@hotmail.com>
2023-06-29 21:31:32 -0300

    Created the comment part and styled it

---
+++ src/components/Comment.jsx
@@ -0,0 +1,32 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/AlissonKelvin.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Alisson Kelvin de Carvalho</strong>
+                            <time title='26 de Junho às 19:42:00' dateTime='2023-06-26 20:42:00'>Cerca de 1h atrás</time>
+                        </div>
+                        <button title='Deletar comentário'>
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p></p>
+
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,5 @@
+import { Comment } from './Comment'
+
 import styles from './Post.module.css'
 
 export function Post() {
@@ -47,6 +49,9 @@ export function Post() {
                 </footer>
 
             </form>
+            <div className={styles.commentList}>
+                <Comment />
+            </div>
         </article>
 
     )
